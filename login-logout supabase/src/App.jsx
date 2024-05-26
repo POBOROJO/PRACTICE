26c71253facd2680c3441ce8ba905cdcb5ebfc2a
POBOROJO <parijatbhatt1@gmail.com>
2024-05-27 02:01:55 +0530

    Revert "Temporary commit for reverting stash@{0}"
    This reverts commit 9e84650f42190af69259fc45c0a45b37a7027a85.

--- login-logout supabase/src/App.jsx
+++ login-logout supabase/src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Home from './pages/Home';
 function App() {
 
   return (
@@ -12,9 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/home" element={<Home/>}></Route>
       </Routes>
-      <ToastContainer />
     </BrowserRouter>
   )
 }
