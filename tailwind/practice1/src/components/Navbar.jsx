dd67d6ea5ab9f71d79a66b3ab4625a1f28e9bcd3
POBOROJO <parijatbhatt1@gmail.com>
2024-09-07 14:17:24 +0530

    Added:comments in the file

--- tailwind/practice1/src/components/Navbar.jsx
+++ tailwind/practice1/src/components/Navbar.jsx
@@ -14,10 +14,12 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
+          {/* logo */}
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
             <span className="text-xl tracking-tight">VirtualR</span>
           </div>
+          {/* list */}
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li key={index}>
@@ -25,10 +27,12 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          {/* Sign In button */}
           <div className="hidden lg:flex justify-center space-x-12 items-center">
             <a href="#" className="py-2 px-3 border rounded-md">
               Sign In
             </a>
+            {/* Sign Up button */}
             <a
               href="#"
               className="bg-gradient-to-r from-blue-500 to-blue-800 py-2 px-3 rounded-md"
@@ -36,6 +40,7 @@ const Navbar = () => {
               Create an account
             </a>
           </div>
+          {/* Menu button */}
           <div className="lg:hidden md:flex flex-col justify-end">
             <button onClick={toggleNavbar}>
               {mobileDrawerOpen ? <X /> : <Menu />}
